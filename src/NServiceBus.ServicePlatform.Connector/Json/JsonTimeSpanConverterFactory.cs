b22b236f6fef0dee2b59d72f2812cfaf80c1822b
David Boike <david.boike@gmail.com>
2022-05-20 15:48:56 -0500

    net6.0

--- src/NServiceBus.ServicePlatform.Connector/Json/JsonTimeSpanConverterFactory.cs
+++ src/NServiceBus.ServicePlatform.Connector/Json/JsonTimeSpanConverterFactory.cs
@@ -11,10 +11,12 @@
             typeToConvert == typeof(TimeSpan)
             || (typeToConvert.IsGenericType && Nullable.GetUnderlyingType(typeToConvert) == typeof(TimeSpan));
 
+#pragma warning disable IDE0004 // Remove Unnecessary Cast - Needed for net472 only
         public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) =>
             typeToConvert.IsGenericType
                 ? (JsonConverter)new NullableJsonTimeSpanConverter()
                 : new JsonTimeSpanConverter();
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
 
         class JsonTimeSpanConverter : JsonConverter<TimeSpan>
         {
@@ -30,7 +32,9 @@
             public override TimeSpan? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 var s = reader.GetString();
+#pragma warning disable IDE0004 // Remove Unnecessary Cast - Needed only for net472
                 return s == null ? (TimeSpan?)null : TimeSpan.ParseExact(s, "c", CultureInfo.CurrentCulture);
+#pragma warning restore IDE0004 // Remove Unnecessary Cast
             }
 
             public override void Write(Utf8JsonWriter writer, TimeSpan? value, JsonSerializerOptions options) => throw new NotImplementedException();
