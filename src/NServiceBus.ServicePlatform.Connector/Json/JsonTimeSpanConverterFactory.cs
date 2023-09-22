aaa94bb1a23f2e544892078065ad4fe84292444a
Brandon Ording <bording@gmail.com>
2023-09-22 13:52:05 -0400

    Prepare repo for .NET 8  (#224)
    * Bump major version
    
    * Update to NServiceBus 9
    
    * Remove pragmas
    
    * Replace Guard with built-in throw helpers
    
    * Update workflows

--- src/NServiceBus.ServicePlatform.Connector/Json/JsonTimeSpanConverterFactory.cs
+++ src/NServiceBus.ServicePlatform.Connector/Json/JsonTimeSpanConverterFactory.cs
@@ -11,12 +11,10 @@
             typeToConvert == typeof(TimeSpan)
             || (typeToConvert.IsGenericType && Nullable.GetUnderlyingType(typeToConvert) == typeof(TimeSpan));
 
-#pragma warning disable IDE0004 // Remove Unnecessary Cast - Needed for net472 only
         public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) =>
             typeToConvert.IsGenericType
-                ? (JsonConverter)new NullableJsonTimeSpanConverter()
+                ? new NullableJsonTimeSpanConverter()
                 : new JsonTimeSpanConverter();
-#pragma warning restore IDE0004 // Remove Unnecessary Cast
 
         class JsonTimeSpanConverter : JsonConverter<TimeSpan>
         {
@@ -32,9 +30,7 @@
             public override TimeSpan? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 var s = reader.GetString();
-#pragma warning disable IDE0004 // Remove Unnecessary Cast - Needed only for net472
-                return s == null ? (TimeSpan?)null : TimeSpan.ParseExact(s, "c", CultureInfo.CurrentCulture);
-#pragma warning restore IDE0004 // Remove Unnecessary Cast
+                return s == null ? null : TimeSpan.ParseExact(s, "c", CultureInfo.CurrentCulture);
             }
 
             public override void Write(Utf8JsonWriter writer, TimeSpan? value, JsonSerializerOptions options) => throw new NotImplementedException();
